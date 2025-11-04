ae3b5cb3331a12551ce8473d50225c5766ecd523
Vasu.dev <vasudev.1160@zenmonk.tech>
2025-11-04 16:29:56 +0530

    feature: updated login api route

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -5,7 +5,7 @@ export const POST = async (request: Request) => {
   const BASE_URL = process.env.NEXT_PUBLIC_SSO_URL;
   const data = await request.json();
   try {
-    const response = await axios.post(`${BASE_URL}/users/verify`, data);
+    const response = await axios.post(`${BASE_URL}/login`, data);
     return NextResponse.json(response.data);
   } catch (error :any) {
     return NextResponse.json(

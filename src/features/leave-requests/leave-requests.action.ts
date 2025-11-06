f322a87e1d65918bab791d06f8de9410a3dcb40f
Arshdev <arshdevrajput@gmail.com>
2025-11-06 17:38:11 +0530

    feat: enhanced LeaveRequestModal with structured form handling, validation, and dynamic components for leave requests.

--- src/features/leave-requests/leave-requests.action.ts
+++ src/features/leave-requests/leave-requests.action.ts
@@ -1,12 +1,50 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { AxiosError } from "axios";
-import { getLeaveRequests } from "./leave-requests.service";
+import {
+  createUserLeaveRequests,
+  getLeaveRequests,
+  getUserLeaveRequests,
+} from "./leave-requests.service";
 
 export const getLeaveRequestsAction = createAsyncThunk(
   "orgnization/leave-requests",
   async (data: any, thunkAPI) => {
     try {
-      const response = await getLeaveRequests(data, data.org_uuid);
+      const response = await getLeaveRequests(data.org_uuid, data);
+      return response.data;
+    } catch (err) {
+      const error = err as AxiosError;
+      return thunkAPI.rejectWithValue(error.response?.data);
+    }
+  }
+);
+
+export const getUserLeaveRequestsAction = createAsyncThunk(
+  "orgnization/user-leave-requests",
+  async (data: any, thunkAPI) => {
+    try {
+      const response = await getUserLeaveRequests(
+        data.org_uuid,
+        data.user_uuid,
+        data
+      );
+      return response.data;
+    } catch (err) {
+      const error = err as AxiosError;
+      return thunkAPI.rejectWithValue(error.response?.data);
+    }
+  }
+);
+
+export const createUserLeaveRequestsAction = createAsyncThunk(
+  "orgnization/create-user-leave-requests",
+  async (data: any, thunkAPI) => {
+    try {
+      const response = await createUserLeaveRequests(
+        data.org_uuid,
+        data.user_uuid,
+        data
+      );
       return response.data;
     } catch (err) {
       const error = err as AxiosError;
---
+++ src/features/leave-requests/leave-requests.types.ts
@@ -0,0 +1,15 @@
+export enum LeaveRange {
+  FULL_DAY = "full_day",
+  FIRST_HALF = "first_half",
+  SECOND_HALF = "second_half",
+  FIRST_QUARTER = "first_quarter",
+  SECOND_QUARTER = "second_quarter",
+  THIRD_QUARTER = "third_quarter",
+  FOURTH_QUARTER = "fourth_quarter",
+}
+
+export enum LeaveRequestType {
+  FULL_DAY = "full_day",
+  HALF_DAY = "half_day",
+  SHORT_LEAVE = "short_leave",
+}

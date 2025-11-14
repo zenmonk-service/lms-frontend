7ffb9351f26ca4b0914412c1b0c722b32f70748b
Ankit Kumar Gupta <ankitkumar2003b@gmail.com>
2025-11-14 11:49:53 +0530

    fix: improve permission checks and formatting in middleware for better readability and functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,22 @@ import { getSession } from "./app/auth/get-auth.action";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
-  const  loggedInUser = await getSession();
+  const loggedInUser = await getSession();
+  console.log("✌️loggedInUser --->", loggedInUser);
 
   if (!loggedInUser) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
   async function hasPagePermission(tag: string) {
-    return (await getSession())?.user?.permissions?.some((perm) => perm.tag === tag);
+    return (await getSession())?.user?.permissions?.some(
+      (perm) => perm.tag === tag
+    );
   }
 
   async function isApprovalPageAccessible() {
     return (
-      await hasPagePermission("leave_request_management") &&
+      (await hasPagePermission("leave_request_management")) &&
       loggedInUser?.user?.permissions?.some(
         (perm) =>
           perm.tag === "leave_request_management" && perm.action === "approve"
@@ -25,11 +28,11 @@ export async function middleware(request: NextRequest) {
 
   async function isMyLeavePageAccessible() {
     return (
-      await hasPagePermission("leave_request_management") &&
+      (await hasPagePermission("leave_request_management")) &&
       loggedInUser?.user?.permissions?.some(
         (perm) =>
           (perm.tag === "leave_request_management" &&
-          perm.action === "create") ||
+            perm.action === "create") ||
           perm.action === "read" ||
           perm.action === "update" ||
           perm.action === "delete"
@@ -39,11 +42,10 @@ export async function middleware(request: NextRequest) {
 
   async function isMyLeaveTypePageAccessible() {
     return (
-      await hasPagePermission("leave_type_management") &&
+      (await hasPagePermission("leave_type_management")) &&
       loggedInUser?.user?.permissions?.some(
         (perm) =>
-          (perm.tag === "leave_type_management" &&
-          perm.action === "create") ||
+          (perm.tag === "leave_type_management" && perm.action === "create") ||
           perm.action === "read" ||
           perm.action === "update" ||
           perm.action === "delete"
@@ -51,34 +53,41 @@ export async function middleware(request: NextRequest) {
     );
   }
 
+  if (
+    loggedInUser.user.email !== "superadmin@superadmin.in" &&
+    request.nextUrl.pathname.startsWith("/organizations")
+  ) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   if (
-    !await hasPagePermission("user_management") &&
+    !(await hasPagePermission("user_management")) &&
     request.nextUrl.pathname.endsWith("/user-management")
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
   if (
-    !await hasPagePermission("role_management") &&
+    !(await hasPagePermission("role_management")) &&
     request.nextUrl.pathname.endsWith("/role-management")
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   if (
-    !await isApprovalPageAccessible() &&
+    !(await isApprovalPageAccessible()) &&
     request.nextUrl.pathname.endsWith("/approvals")
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   if (
-    !await isMyLeaveTypePageAccessible() &&
+    !(await isMyLeaveTypePageAccessible()) &&
     request.nextUrl.pathname.endsWith("/leave-types")
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
   if (
-    !await isMyLeavePageAccessible() &&
+    !(await isMyLeavePageAccessible()) &&
     request.nextUrl.pathname.endsWith("/my-leaves")
   ) {
     return NextResponse.redirect(new URL("/login", request.url));
@@ -90,6 +99,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     "/users/:path*",
+    "/organizations/:path*",
     "/select-organization",
     "/:org/user-management",
     "/:org/role-management",

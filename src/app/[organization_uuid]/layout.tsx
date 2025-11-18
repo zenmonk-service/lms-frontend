a84719740651a44a6d967559eb60d413ba208df2
Ankit Kumar Gupta <ankitkumar2003b@gmail.com>
2025-11-18 16:14:24 +0530

    fix: implement organization redirection logic based on user session

--- src/app/[organization_uuid]/layout.tsx
+++ src/app/[organization_uuid]/layout.tsx
@@ -2,12 +2,18 @@ import AppBar from "@/components/app-bar";
 import { AppSidebar } from "@/components/layout/sidebar";
 import { SiteHeader } from "@/components/layout/site-header";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { getSession } from "../auth/get-auth.action";
+import { redirect } from "next/navigation";
 
 export default async function Layout({ children, params }: any) {
   const resolvedParams = await Promise.resolve(params);
   const organization_uuid = Array.isArray(resolvedParams.organization_uuid)
     ? resolvedParams.organization_uuid[0]
     : resolvedParams.organization_uuid;
+  const session = await getSession();
+  if (session && session.user.org_uuid !== organization_uuid) {
+    redirect("/select-organization");
+  }
 
   return (
     <div className="h-screen flex flex-col">
